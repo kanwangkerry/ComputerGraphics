5c0a0640fb45dca2e6601e6c2b7d83d5b43559d7
Kan Wang <kw1308@nyu.edu>
2013-02-26 22:54:29 -0500

    hw 4 commit 1

---
+++ src/org/hw4/SimplePersone.java
@@ -0,0 +1,18 @@
+package org.hw4;
+
+import java.awt.Graphics;
+
+import org.utils.BufferedApplet;
+
+@SuppressWarnings("serial")
+public class SimplePersone extends BufferedApplet {
+
+	
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
