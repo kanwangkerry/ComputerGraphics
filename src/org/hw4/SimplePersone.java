226d56a2a560e07d0ae5a607d5198ab48d1ee364
Kan Wang <kw1308@nyu.edu>
2013-02-27 16:25:31 -0500

    hw 4 person is not finished

--- src/org/hw4/SimplePersone.java
+++ src/org/hw4/SimplePersone.java
@@ -6,6 +6,7 @@ import java.awt.Graphics;
 import org.utils.BufferedApplet;
 import org.utils.shape.Geometry;
 import org.utils.shape.ShapeCube;
+import org.utils.shape.ShapeCylinder;
 import org.utils.shape.ShapeSphere;
 import org.utils.transform.Projection;
 
@@ -13,22 +14,55 @@ import org.utils.transform.Projection;
 public class SimplePersone extends BufferedApplet {
 
 	int w = 0, h = 0;
+	
+
+	double speedCirclePerSecond = 0.5;
+	int framePerSec = 24;
+	int frameCount = 0;
+	double alpha;
+	
+	int timeInterval = 1000;
+	long lastTime = 0;
+	int caseAnimate = 0;
+	
 	Projection proj = new Projection();
+	Person p;
 	
 	@Override
 	public void render(Graphics g) {
 		if (w == 0) {
 			w = getWidth();
 			h = getHeight();
-			proj = new Projection(w, h, 10.0);
+			proj = new Projection(w, h, 15.0);
+			p = new Person();
+			alpha = 0;
+			lastTime = System.currentTimeMillis();
 		}
 		
 		g.setColor(Color.white);
 		g.fillRect(0, 0, w, h);
 		g.setColor(Color.black);
-		Geometry cube = new ShapeCube();
-		cube.globe(20, 20);
-		cube.drawShape(g, proj);
+//		Geometry cube = new ShapeCylinder();
+//		cube.globe(20, 20);
+//		//cube.getMatrix().scale(3, 3, 3);
+//		cube.getMatrix().rotateX(-1);
+//
+//		cube.drawShape(g, proj);
+
+		
+		
+		long ctime = System.currentTimeMillis();
+		if(ctime - lastTime > timeInterval){
+			lastTime = ctime;
+			caseAnimate = (caseAnimate+1)%7;
+		}
+		
+		alpha = speedCirclePerSecond*Math.PI*((frameCount)/(double)framePerSec);
+		frameCount = (frameCount+1+framePerSec*2+1)%(framePerSec*2+1);
+
+		p.move(frameCount/(double)framePerSec);
+		p.drawPerson(g, proj);
+
 
 	}
 
